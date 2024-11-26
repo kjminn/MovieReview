b2a4163fcb70faf523efbf6c23aaf46833639394
kjminn <kjminn2001@gmail.com>
2024-11-26 16:30:21 +0900

    영화 리뷰 프로젝트 Entity Test(Movie, MovieImage)

---
+++ src/test/java/kr/ac/kopo/moviereview/repository/MovieRepositoryTest.java
@@ -0,0 +1,42 @@
+package kr.ac.kopo.moviereview.repository;
+
+import kr.ac.kopo.moviereview.entity.Movie;
+import kr.ac.kopo.moviereview.entity.MovieImage;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class MovieRepositoryTest {
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @Autowired
+    private MovieImageRepository movieImageRepository;
+
+    @Test
+    public  void insertMovies(){
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+            Movie movie = Movie.builder()
+                    .title("Movie Test " + i)
+                    .build();
+
+            movieRepository.save(movie);
+
+            int imgCount = (int)(Math.random() * 5) + 1;
+
+            for (int j=0; j < imgCount; j++){
+                MovieImage movieImage = MovieImage.builder()
+                        .uuid(UUID.randomUUID().toString())
+                        .movie(movie)
+                        .imgName("test"+j+".jpg")
+                        .build();
+                movieImageRepository.save(movieImage);
+            }
+        });
+    }
+
+}
